624c5773749e27557720569f11caedf0965f33c6
Srinath <srinathv1001@gmail.com>
2018-06-14 15:57:16 +0530

    scroll to top button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import { AppRegistry } from "react-native";
 import App from "./App";
-import LazyLoadImage from './components/LazyLoadImage.jsx';
-import LazyLoadComponent from './components/LazyLoadComponent.jsx';
-import trackWindowScroll from './hoc/trackWindowScroll.js';
 
 AppRegistry.registerComponent("App", () => App);
 
 AppRegistry.runApplication("App", {
   rootTag: document.getElementById("root")
 });
-export { LazyLoadImage, LazyLoadComponent, trackWindowScroll };
