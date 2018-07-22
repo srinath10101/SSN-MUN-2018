350f9df777ea793e91973eba0bdd5776c73ab9aa
Srinath <srinathv1001@gmail.com>
2018-07-22 19:41:18 +0530

    tech and design

--- src/components/Sec.jsx
+++ src/components/Sec.jsx
@@ -332,7 +332,6 @@ class Sec extends Component {
                 </RevealP>
                     }
                 </WhenInView>
-                <div className="col-xs-12"><br></br><br></br></div>
                 <WhenInView>
                     {({ isInView }) =>
                         <RevealP hide={!isInView}>
