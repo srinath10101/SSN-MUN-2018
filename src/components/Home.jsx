3be52833c75813650535a56e7cd7abbcda38b8d3
Srinath <srinathv1001@gmail.com>
2018-07-25 19:35:43 +0530

    font added home.jsx page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,7 +34,7 @@ export default class Home extends Component {
         <ScrollUpButton/>
                   <div><br></br><br></br></div>      
                   <Grid id="whole">
-                  <div className="col-lg-12" style={{border: "medium solid black", background: "url('images/hd2flippedmob.jpg')"}}><h3 style={{color: "black", textAlign: "center"}}><b>Letter from the<br></br> Secretary General</b></h3><p style={{color: "black"}}>
+                  <div className="col-lg-12" style={{border: "medium solid black", background: "url('images/hd2flippedmob.jpg')"}}><h3 id = "heading" style={{color: "black", textAlign: "center", fontSize: "22px"}}><b>Letter from the<br></br> Secretary General</b></h3><p style={{color: "black"}}>
                   <br></br>
                   <Image src="./images/anandhomepage.jpg" style={{position: "relative", width: "100%", height: "100%"}}/>
                   <WhenInView>
@@ -42,20 +42,17 @@ export default class Home extends Component {
                         <RevealP hide={!isInView}>    
                   <br></br>
                   <br></br>
+                  <p id="letter">
                   Ladies and Gentlemen,
   <br></br><br></br>
-  On behalf of the entire Organizing Committee, it gives me great pleasure to welcome you to SSN Model United Nations, 2018.<br></br>
-  <br></br>
-  Ever since its inception in 2014, SSNMUN has been renown for the standards that it has established in the MUN circuit in terms of the quality of the participation of delegates and the Executive Board members who have been a part of our conference. This conference has always sought to build upon and surpass  the foundations and standards that we have established with each subsequent edition and we’re confident this edition will be no different. <br></br>
-  <br></br>
-  This year, with a total of 6 committees, we look forward to hosting a conference of exceptional quality, and ensure that all attendees take away something of value, enriching all stakeholders in the process as well as making it a conference to remember.<br></br>
-  <br></br>
-  Henceforth, I cordially invite you to SSN MUN 2018, to experience three days of engaging debate and deliberation.
-  <br></br><br></br>
+  On behalf of the entire Organizing Committee, it gives me great pleasure to welcome you to SSN Model United Nations, 2018.<br></br><br></br>
+  Ever since its inception in 2014, SSNMUN has been renown for the standards that it has established in the MUN circuit in terms of the quality of the participation of delegates and the Executive Board members who have been a part of our conference. This conference has always sought to build upon and surpass  the foundations and standards that we have established with each subsequent edition and we’re confident this edition will be no different. <br></br><br></br>
+  This year, with a total of 6 committees, we look forward to hosting a conference of exceptional quality, and ensure that all attendees take away something of value, enriching all stakeholders in the process as well as making it a conference to remember.<br></br><br></br>
+  Henceforth, I cordially invite you to SSN MUN 2018, to experience three days of engaging debate and deliberation.<br></br><br></br>
   Yours Sincerely<br></br>
   Anand Subramanian<br></br>
   Secretary General<br></br>
-  SSN MUN 2018<br></br>
+  SSN MUN 2018<br></br></p>
   </RevealP>
                     }
                 </WhenInView>
@@ -105,7 +102,7 @@ export default class Home extends Component {
           <ScrollUpButton/>
                     <div><br></br><br></br></div>      
                     <Grid id="whole">
-                    <div className="col-lg-12" style={{border: "medium solid black", background: "url('images/hd2flipped.jpg')"}}><h2 style={{color: "black", textAlign: "center"}}><b>Letter from the Secretary General</b></h2><p style={{color: "black"}}>
+                    <div className="col-lg-12" style={{border: "medium solid black", background: "url('images/hd2flipped.jpg')"}}><h1 id="heading" style={{color: "black", textAlign: "center"}}><b>Letter from the Secretary General</b></h1><p style={{color: "black"}}>
                     <div className="col-lg-6 col-lg-offset-3">
                     <Image src="./images/anandhomepage.jpg" style={{position: "relative", width: "100%", border: "medium solid black", height: "100%"}}/>
                     </div>
@@ -113,18 +110,19 @@ export default class Home extends Component {
                     <br></br><br></br>
                 <WhenInView>
                     {({ isInView }) =>
-                        <RevealP hide={!isInView}>                                      
-                    Ladies and Gentlemen,
-    <br></br>
-    On behalf of the entire Organizing Committee, it gives me great pleasure to welcome you to SSN Model United Nations, 2018.<br></br>
-    Ever since its inception in 2014, SSNMUN has been renown for the standards that it has established in the MUN circuit in terms of the quality of the participation of delegates and the Executive Board members who have been a part of our conference. This conference has always sought to build upon and surpass  the foundations and standards that we have established with each subsequent edition and we’re confident this edition will be no different. <br></br>
-    This year, with a total of 6 committees, we look forward to hosting a conference of exceptional quality, and ensure that all attendees take away something of value, enriching all stakeholders in the process as well as making it a conference to remember.<br></br>
+                        <RevealP hide={!isInView}>
+                        <p id="letter">                                      
+                    Ladies and Gentlemen,<br></br><br></br>
+    On behalf of the entire Organizing Committee, it gives me great pleasure to welcome you to SSN Model United Nations, 2018.<br></br><br></br>
+    Ever since its inception in 2014, SSNMUN has been renown for the standards that it has established in the MUN circuit in terms of the quality of the participation of delegates and the Executive Board members who have been a part of our conference. This conference has always sought to build upon and surpass  the foundations and standards that we have established with each subsequent edition and we’re confident this edition will be no different. <br></br><br></br>
+    This year, with a total of 6 committees, we look forward to hosting a conference of exceptional quality, and ensure that all attendees take away something of value, enriching all stakeholders in the process as well as making it a conference to remember.<br></br><br></br>
     Henceforth, I cordially invite you to SSN MUN 2018, to experience three days of engaging debate and deliberation.
-    <br></br>
+    <br></br><br></br>
     Yours Sincerely<br></br>
     Anand Subramanian<br></br>
     Secretary General<br></br>
     SSN MUN 2018<br></br><br></br>
+    </p>
     </RevealP>
                     }
                 </WhenInView>
