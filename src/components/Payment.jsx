3079705bb4ba4b7ed6face69a689029c701b64b2
Srinath <srinathv1001@gmail.com>
2018-08-23 10:49:43 +0530

    payment redirect

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -13,38 +13,9 @@ export default class Payment extends Component{
     }
     setbgimg()
     {
-        var screenWidth = window.innerWidth;
-      if( screenWidth<768){
+        //var screenWidth = window.innerWidth;
             window.location = "http://www.ssn.edu.in/apps/mun-payment-form/";
             return;
-      }
-      else{
-        var screenheight = window.innerHeight;
-        screenheight = (screenheight) + "px";
-
-            return(
-
-                <View style={{backgroundColor: "grey", scrollX:"enabled"}}>
-                <Grid>
-                <View style={{width:"100%", height: screenheight}}><iframe src="http://www.ssn.edu.in/apps/mun-payment-form/" width="100%" height="100%" style={{marginTop: "3.5em"}} frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe></View>
-                </Grid>
-                <View
-      style={{
-        borderBottomColor: 'grey',
-        borderBottomWidth: 3,
-      }}
-    />
-          <div style={{backgroundColor: "#bfbab9", color:"black", textAlign: "center", fontSize: "15px" }}><b>Made with:</b>
-          <br></br>
-          <Image src="./images/reactimg.png" style={{position: "relative", width: "60px", height: "60px"}}/>
-              <b>and</b> {/*<Image src="images/addimg.png" style={{position: "relative", width: "50px", height: "50px"}}/>*/}
-          <Image src="images/heartimg.png" style={{position: "relative", width: "40px", height: "40px"}}/>
-          <br></br>
-    </div>
-            </View>
-
-            );
-      }
     }
     render() {
         return (
